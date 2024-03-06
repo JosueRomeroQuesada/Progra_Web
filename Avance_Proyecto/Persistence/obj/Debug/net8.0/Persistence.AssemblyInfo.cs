c7c1df618afc49d8afd9205ddd9e5d6b81ca03a8
JosueRomeroQuesada <107214540+JosueRomeroQuesada@users.noreply.github.com>
2024-03-06 13:20:11 -0600

    Create y delete de client

--- Avance_Proyecto/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Avance_Proyecto/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+404846cde65a972e6cdd8a34d2474304bf8bb89a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
