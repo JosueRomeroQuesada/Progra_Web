12e5d227e51d3cdf02bd9a437e05b760cfa2afed
Diegoespiher <diegoespinach12@gmail.com>
2024-04-16 22:14:46 -0600

    index, create y update

--- ProyectoGim/Domain/Clients/ClientDTO.cs
+++ ProyectoGim/Domain/Clients/ClientDTO.cs
@@ -10,11 +10,14 @@ namespace Domain.Clients
     {
         public ClientDTO() { }
 
-        public ClientDTO(int id, string idCliente, string nombreCompleto) 
+        public ClientDTO(int id, string idCliente, string nombreCompleto, string peso, string altura, string preferencias) 
         {
             Id = id;
             IdCliente = idCliente;
             NombreCompleto = nombreCompleto;
+            Peso = peso;
+            Altura = altura;
+            Preferencias = preferencias;
         }
 
         public int Id { get; set; }
@@ -22,5 +25,8 @@ namespace Domain.Clients
         public string IdCliente { get; set; }
 
         public string NombreCompleto { get; set; }
+        public string Peso { get; }
+        public string Altura { get; }
+        public string Preferencias { get; }
     }
 }
--- ProyectoGim/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ ProyectoGim/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e45d7c729db143d2df6778dbda995b2634e2be6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63edf9083232e87f71e0b349badfb044d036d21c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
