32227c066a191a924b6ed0d4b2e5020adbe8c708
Diegoespiher <diegoespinach12@gmail.com>
2024-04-22 21:37:31 -0600

    uPDATE2

--- ProyectoGim/Domain/Instructors/InstructorDTO.cs
+++ ProyectoGim/Domain/Instructors/InstructorDTO.cs
@@ -10,17 +10,18 @@ namespace Domain.Instructors
     {
         public InstructorDTO() { }
 
-        public InstructorDTO(int id, string idEntrenador, string nombreCompleto) 
+        public InstructorDTO(int id, string idEntrenador, string nombreCompleto, string cedula) 
         {
             Id = id;
             IdEntrenador = idEntrenador;
+            Cedula = cedula;
             NombreCompleto = nombreCompleto;
         }
 
         public int Id { get; set; }
 
         public string IdEntrenador { get; set; }
-
+        public string Cedula { get; set; }
         public string NombreCompleto { get; set; }
     }
 }
--- ProyectoGim/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ ProyectoGim/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63edf9083232e87f71e0b349badfb044d036d21c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efe9f9215ded25297de85b5ab8d2a4b6f125bf95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
