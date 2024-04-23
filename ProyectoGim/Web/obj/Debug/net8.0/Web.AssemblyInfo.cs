7c0e2b05165ecbf7575b73073469001f3838f4d1
Diegoespiher <diegoespinach12@gmail.com>
2024-04-22 21:39:55 -0600

    Instructors

--- ProyectoGim/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ ProyectoGim/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efe9f9215ded25297de85b5ab8d2a4b6f125bf95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32227c066a191a924b6ed0d4b2e5020adbe8c708")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
